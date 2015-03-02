94651bc2e5adec1464870220c4a3c3540b1a466d
Anthony Leonardo Gracio <anthony.gracio@gmail.com>
2015-03-02 17:27:56 +0100

    FreeRTOS functions put in a different package

---
+++ wrappers/ada_spark/spec/FreeRTOS_pack.ads
@@ -0,0 +1,25 @@
+with System; use System;
+with Interfaces; use Interfaces;
+with Interfaces.C; use Interfaces.C;
+
+package FreeRTOS_Pack is
+   --  Types
+   subtype Pvoid is System.Address;
+
+   --  Functions and procedures
+   function XQueue_Create(QueueLength : Unsigned_32;
+                          ItemSize    : Integer) return Pvoid;
+   pragma Import(C, XQueue_Create, "w_xQueueCreate");
+
+   function XQueue_Receive(XQueue        : Pvoid;
+                           Buffer        : Pvoid;
+                           Ticks_To_wait : Unsigned_32) return Integer;
+   pragma Import(C, XQueue_Receive, "w_xQueueReceive");
+
+   function XQueue_Send(XQueue        : Pvoid;
+                           Item_To_Queue : PVoid;
+                           Ticks_To_wait : Unsigned_32) return Integer;
+   pragma Import(C, XQueue_Send, "w_xQueueSend");
+
+
+end FreeRTOS_Pack;
