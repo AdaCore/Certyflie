c0ee5ae429710257e5c98a3a952a8f2298c6cf8a
Anthony Leonardo Gracio <anthony.gracio@gmail.com>
2015-04-03 18:02:28 +0200

    Add new debug facilities than can be called from Ada code

--- ada_spark/utils/spec/debug_pack.ads
+++ ada_spark/utils/spec/debug_pack.ads
@@ -5,6 +5,8 @@ package Debug_Pack is
 
    --  Functions and procedures
 
+   procedure Debug_Print (Str : String);
+
    --  Wrapper for the 'consolePuts' function
    --  declared in 'utils/interface/debug.h'
    function C_ConsolePuts (Str : char_array) return Integer;
@@ -14,5 +16,14 @@ package Debug_Pack is
    --  declared in 'utils/interface/debug.h'
    function C_ConsolePutchar (Ch : Integer) return Integer;
    pragma Import (C, C_ConsolePutchar, "consolePutchar");
+
+private
+
+   function To_C   (Item : Character) return char;
+
+   function To_C
+     (Item       : String;
+      Append_Nul : Boolean := True) return char_array;
+
 
 end Debug_Pack;
