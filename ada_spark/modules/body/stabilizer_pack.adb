240de8e48f3225cc87d30805d73e521d6a058616
Anthony Leonardo Gracio <leonardo@adacore.com>
2016-02-23 13:52:47 +0100

    Add licence headers to all Ada/SPARK source files

--- ada_spark/modules/body/stabilizer_pack.adb
+++ ada_spark/modules/body/stabilizer_pack.adb
@@ -1,9 +1,38 @@
+------------------------------------------------------------------------------
+--                              Certyflie                                   --
+--                                                                          --
+--                     Copyright (C) 2015-2016, AdaCore                     --
+--                                                                          --
+--  This library is free software;  you can redistribute it and/or modify   --
+--  it under terms of the  GNU General Public License  as published by the  --
+--  Free Software  Foundation;  either version 3,  or (at your  option) any --
+--  later version. This library is distributed in the hope that it will be  --
+--  useful, but WITHOUT ANY WARRANTY;  without even the implied warranty of --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                    --
+--                                                                          --
+--  As a special exception under Section 7 of GPL version 3, you are        --
+--  granted additional permissions described in the GCC Runtime Library     --
+--  Exception, version 3.1, as published by the Free Software Foundation.   --
+--                                                                          --
+--  You should have received a copy of the GNU General Public License and   --
+--  a copy of the GCC Runtime Library Exception along with this program;    --
+--  see the files COPYING3 and COPYING.RUNTIME respectively.  If not, see   --
+--  <http://www.gnu.org/licenses/>.                                         --
+--                                                                          --
+--  As a special exception, if other files instantiate generics from this   --
+--  unit, or you link this unit with other files to produce an executable,  --
+--  this  unit  does not  by itself cause  the resulting executable to be   --
+--  covered by the GNU General Public License. This exception does not      --
+--  however invalidate any other reasons why the executable file  might be  --
+--  covered by the  GNU Public License.                                     --
+------------------------------------------------------------------------------
+
 with Interfaces.C; use Interfaces.C;
 
-with Config; use Config;
-with Safety_Pack; use Safety_Pack;
-with Motors_Pack; use Motors_Pack;
-with PM_Pack; use PM_Pack;
+with Config;       use Config;
+with Motors_Pack;  use Motors_Pack;
+with PM_Pack;      use PM_Pack;
+with Safety_Pack;  use Safety_Pack;
 
 package body Stabilizer_Pack
 with SPARK_Mode,
