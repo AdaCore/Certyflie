9d2ce4f8b18f4b997582cedc9d50901d0ce1da2e
Simon Wright <simon@pushface.org>
2017-08-01 21:00:16 +0100

    Updated copyright date.
      * drivers/uart_syslink.adb: to 2017.

--- drivers/uart_syslink.adb
+++ drivers/uart_syslink.adb
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Certyflie                                   --
 --                                                                          --
---                     Copyright (C) 2015-2016, AdaCore                     --
+--                     Copyright (C) 2015-2017, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
