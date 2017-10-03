48e4e2fbb58f89a146f1077e73aa302b25c935b3
Simon Wright <simon@pushface.org>
2017-10-03 10:57:52 +0100

    Implement flow control of DMA transfers.
    Note 1: Uses updated & modified STM32.DMA.Interrupts.
    Note 2: This is unfinished. Stops working after a minute or two.
    
      * drivers/uart_syslink.ads: Moved private parts, and context clauses
          to support them, to body.
      * drivers/uart_syslink.adb: Implemented flow control of DMA
          transfers. Flash the left red LED on receive overrun. Flash the
          right red LED when a DMA transfer is suspended for flow control.

--- crazyflie_support/src/uart_syslink.ads
+++ crazyflie_support/src/uart_syslink.ads
@@ -1,7 +1,7 @@
 ------------------------------------------------------------------------------
 --                              Certyflie                                   --
 --                                                                          --
---                     Copyright (C) 2015-2016, AdaCore                     --
+--                     Copyright (C) 2015-2017, AdaCore                     --
 --                                                                          --
 --  This library is free software;  you can redistribute it and/or modify   --
 --  it under terms of the  GNU General Public License  as published by the  --
@@ -27,17 +27,7 @@
 --  covered by the  GNU Public License.                                     --
 ------------------------------------------------------------------------------
 
-with Ada.Unchecked_Conversion;
-with Ada.Interrupts.Names;     use Ada.Interrupts.Names;
-with Ada.Real_Time;            use Ada.Real_Time;
-
-with STM32;                    use STM32;
-with STM32.Device;             use STM32.Device;
-with STM32.DMA;                use STM32.DMA;
-with STM32.USARTs;             use STM32.USARTs;
-
 with Types;                    use Types;
-with Generic_Queue;
 
 package UART_Syslink is
 
@@ -60,73 +50,5 @@ package UART_Syslink is
    procedure UART_Send_DMA_Data_Blocking
      (Data_Size : Natural;
       Data      : DMA_Data);
-
-private
-
-   package T_Uint8_Queue is new Generic_Queue (T_Uint8);
-   use T_Uint8_Queue;
-
-   --  Global variables and constants
-
-   Controller : DMA_Controller renames DMA_2;
-
-   Tx_Channel : constant DMA_Channel_Selector := Channel_5;
-   Tx_Stream : constant DMA_Stream_Selector := Stream_6;
-
-   DMA_Tx_IRQ : constant Ada.Interrupts.Interrupt_ID := DMA2_Stream6_Interrupt;
-   --  must match that of the selected controller and stream number!!!!
-
-   UART_RX_QUEUE_SIZE   : constant := 40;
-   UART_DATA_TIMEOUT_MS : constant Time_Span :=  Milliseconds (1_000);
-
-   --  Procedures and functions
-
-   --  Initialize the STM32F4 USART controller.
-   procedure Initialize_USART;
-
-   --  Configure the STM32F4 USART controller.
-   procedure Configure_USART;
-
-   --  Initialize the STM32F4 DMA controller.
-   procedure Initialize_DMA;
-
-   --  Enable USART interrupts in reception.
-   procedure Enable_USART_Rx_Interrupts;
-
-   --  Finalize Tx DMA transmissions.
-   procedure Finalize_DMA_Transmission (Transceiver : in out USART);
-
-   --  Tasks and protected objects
-
-   --  DMA Interrupt Handler for transmission.
-   protected Tx_IRQ_Handler is
-      pragma Interrupt_Priority;
-
-      entry Await_Transfer_Complete;
-
-   private
-
-      Event_Occurred    : Boolean := False;
-      Transfer_Complete : Boolean := False;
-      Event_Kind        : DMA_Interrupt;
-
-      procedure IRQ_Handler;
-      pragma Attach_Handler (IRQ_Handler, DMA_Tx_IRQ);
-   end Tx_IRQ_Handler;
-
-   --  Interrupt Handler for reception (DMA not used here).
-   protected Rx_IRQ_Handler is
-      pragma Interrupt_Priority;
-
-      entry Await_Byte_Reception (Rx_Byte : out T_Uint8);
-
-   private
-
-      Byte_Avalaible  : Boolean := False;
-      Rx_Queue        : T_Queue (UART_RX_QUEUE_SIZE);
-
-      procedure IRQ_Handler;
-      pragma Attach_Handler (IRQ_Handler, USART6_Interrupt);
-   end Rx_IRQ_Handler;
 
 end UART_Syslink;
